8cef38ffd64ba0da79e9637a625481c8986a8366
yuji <yujiricardo@gmail.com>
2024-05-30 10:49:02 -0300

    feat racf

--- api/main.go
+++ api/main.go
@@ -20,6 +20,10 @@ type TokenRequest struct {
 	Token string `json:"token"`
 }
 
+type GetTokenRequest struct {
+	Racf string `json:"racf"`
+}
+
 func main() {
 	redisAddr := os.Getenv("REDIS_ADDR")
 	if redisAddr == "" {
@@ -57,25 +61,25 @@ func main() {
 	})
 
 	http.HandleFunc("/get-token", func(w http.ResponseWriter, r *http.Request) {
-		// Log the request for debugging purposes
+
 		log.Println("Received request for /get-token")
 
-		// Attempt to get the token from Redis
-		token, err := rdb.Get(ctx, "user123").Result()
+		racf := r.URL.Query().Get("racf")
+
+		token, err := rdb.Get(ctx, racf).Result()
+
 		if err == redis.Nil {
 			http.Error(w, "Token not found", http.StatusNotFound)
 			return
 		} else if err != nil {
-			// Log the error for debugging purposes
+
 			log.Printf("Error getting token from Redis: %v", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// Log the successful retrieval of the token
 		log.Println("Token retrieved successfully")
 
-		// Encode the token to JSON and write it to the response
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{
 			"token": token,
